18e369c1bd6ea71f0da00da7a3015e67446a9da3
Subhankito Roy Choudhury <subhankitoroychoudhuryofficial@gmail.com>
2025-06-25 17:15:17 +0530

    Auth

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -3,8 +3,8 @@ import { Account, Client } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // ✅ Correct
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // ✅ Correct
 
 export const account = new Account(client);
 export default client;
